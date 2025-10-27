03fcfac15603567233b6b0172cdb4e90fbcb3ef5
daftpunc <daftpunc8@gmail.com>
2025-10-27 16:06:09 +0600

    daft_commit_korlo

---
+++ practice.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+         string a,b;
+         cout<< "Enter first word"<<endl;
+         cin>>a;
+         cout<<" Enter another word"<<endl;
+         cin >>b;
+         
+         }
+
+
+
+
+ return 0;
+}
+
+
+
